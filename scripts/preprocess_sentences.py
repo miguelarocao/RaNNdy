65dfa482f5fcc6b2a474e32cf688098c4c00decb
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2018-09-08 14:18:04 -0400

    address review comments

--- scripts/preprocess_sentences.py
+++ scripts/preprocess_sentences.py
@@ -2,129 +2,8 @@ import string
 import argparse
 import nltk
 import csv
-import re
-cList = {
-  "ain't": "am not",
-  "aren't": "are not",
-  "can't": "cannot",
-  "can't've": "cannot have",
-  "'cause": "because",
-  "could've": "could have",
-  "couldn't": "could not",
-  "couldn't've": "could not have",
-  "didn't": "did not",
-  "doesn't": "does not",
-  "don't": "do not",
-  "hadn't": "had not",
-  "hadn't've": "had not have",
-  "hasn't": "has not",
-  "haven't": "have not",
-  "he'd": "he would",
-  "he'd've": "he would have",
-  "he'll": "he will",
-  "he'll've": "he will have",
-  "he's": "he is",
-  "how'd": "how did",
-  "how'd'y": "how do you",
-  "how'll": "how will",
-  "how's": "how is",
-  "I'd": "I would",
-  "I'd've": "I would have",
-  "I'll": "I will",
-  "I'll've": "I will have",
-  "I'm": "I am",
-  "I've": "I have",
-  "isn't": "is not",
-  "it'd": "it had",
-  "it'd've": "it would have",
-  "it'll": "it will",
-  "it'll've": "it will have",
-  "it's": "it is",
-  "let's": "let us",
-  "ma'am": "madam",
-  "mayn't": "may not",
-  "might've": "might have",
-  "mightn't": "might not",
-  "mightn't've": "might not have",
-  "must've": "must have",
-  "mustn't": "must not",
-  "mustn't've": "must not have",
-  "needn't": "need not",
-  "needn't've": "need not have",
-  "o'clock": "of the clock",
-  "oughtn't": "ought not",
-  "oughtn't've": "ought not have",
-  "shan't": "shall not",
-  "sha'n't": "shall not",
-  "shan't've": "shall not have",
-  "she'd": "she would",
-  "she'd've": "she would have",
-  "she'll": "she will",
-  "she'll've": "she will have",
-  "she's": "she is",
-  "should've": "should have",
-  "shouldn't": "should not",
-  "shouldn't've": "should not have",
-  "so've": "so have",
-  "so's": "so is",
-  "that'd": "that would",
-  "that'd've": "that would have",
-  "that's": "that is",
-  "there'd": "there had",
-  "there'd've": "there would have",
-  "there's": "there is",
-  "they'd": "they would",
-  "they'd've": "they would have",
-  "they'll": "they will",
-  "they'll've": "they will have",
-  "they're": "they are",
-  "they've": "they have",
-  "to've": "to have",
-  "wasn't": "was not",
-  "we'd": "we had",
-  "we'd've": "we would have",
-  "we'll": "we will",
-  "we'll've": "we will have",
-  "we're": "we are",
-  "we've": "we have",
-  "weren't": "were not",
-  "what'll": "what will",
-  "what'll've": "what will have",
-  "what're": "what are",
-  "what's": "what is",
-  "what've": "what have",
-  "when's": "when is",
-  "when've": "when have",
-  "where'd": "where did",
-  "where's": "where is",
-  "where've": "where have",
-  "who'll": "who will",
-  "who'll've": "who will have",
-  "who's": "who is",
-  "who've": "who have",
-  "why's": "why is",
-  "why've": "why have",
-  "will've": "will have",
-  "won't": "will not",
-  "won't've": "will not have",
-  "would've": "would have",
-  "wouldn't": "would not",
-  "wouldn't've": "would not have",
-  "y'all": "you all",
-  "y'alls": "you alls",
-  "y'all'd": "you all would",
-  "y'all'd've": "you all would have",
-  "y'all're": "you all are",
-  "y'all've": "you all have",
-  "you'd": "you had",
-  "you'd've": "you would have",
-  "you'll": "you you will",
-  "you'll've": "you you will have",
-  "you're": "you are",
-  "you've": "you have"
-}
+import contraction_list as cl
 
-c_re = re.compile('(%s)' % '|'.join(cList.keys()))
 
 def parse_sentences(input_file, token_output_file, vocab_output_file, max_length=32, num_word_print=10):
     """
@@ -154,6 +33,7 @@ def parse_sentences(input_file, token_output_file, vocab_output_file, max_length
                 continue
 
             sentence = rap_word_expansion(line.lower())
+            sentence = expand_contractions(sentence)
 
             words = nltk.word_tokenize(sentence)
 
@@ -186,13 +66,19 @@ def rap_word_expansion(sentence):
     :param sentence: [String] Sentence to expand.
     :return: Expanded sentence.
     """
-    sentence = expand_contractions(sentence)
     return sentence.replace("in' ", "ing ")
 
-def expand_contractions(text, c_re=c_re):
+def expand_contractions(sentence):
+    """
+    Expands common english contractions defined in the contraction_list.py file.
+    cl.regex_form is a regex object that is searched and each occurance of the contraction is 
+    replaced with its uncontracted counterpart.
+    :param sentence: [String] Sentence to expand.
+    :return: Expanded sentence.
+    """
     def replace(match):
-        return cList[match.group(0)]
-    return c_re.sub(replace, text.lower())
+        return cl.contraction_dict[match.group(0)]
+    return cl.compiled_regex.sub(replace, sentence)
 
 def main():
     parser = argparse.ArgumentParser(description="Preprocessing.")
